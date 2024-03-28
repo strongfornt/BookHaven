d353721c797cce0f109b405a75fa4080883c8c01
storngfornt <au0038173@gmail.com>
2024-03-28 10:11:38 +0000

    change some design inr popular books route

--- src/Components/PopularBook/PopularBook.jsx
+++ src/Components/PopularBook/PopularBook.jsx
@@ -11,12 +11,12 @@ export default function PopularBook({popular}) {
           alt=""
           className="object-contain object-center w-full rounded-md   "
         />
-        <div className="mt-1 mb-2 flex flex-col items-center  justify-center">
+        <div className="mt-1 mb-2 flex flex-col items-start lg:items-center  justify-center">
          
-          <h2 className="text-xl font-semibold tracking-wide">
+          <h2 className=" text-base lg:text-xl font-semibold tracking-wide   ">
             {bookName}
           </h2>
-          <p className="text-sm text-[#131313CC]">By: {writerName}</p> 
+          <p className="text-sm text-[#131313CC]   ">By: {writerName}</p> 
           <p className="text-base font-medium mt-4 text-red-800">$: {price}</p> 
         </div>
      
